157084a4b7df35d8937969cd15ded0f8a27ee91a
Jessica Haugen <j.rose.haugen@gmail.com>
2021-08-17 11:43:23 -0500

    dictionary model

--- tictac.py
+++ tictac.py
@@ -4,6 +4,8 @@ board = {
     '1': ' ' , '2': ' ' , '3': ' ' 
 }
 
+keys = []
+
 def displayBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
     print('-+-+-')
@@ -12,4 +14,12 @@ def displayBoard(board):
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
 displayBoard(board)
+
+def play(): 
+    move = "x"
+    count = 0
+
+    for i in range(10):
+        displayBoard(board)
+        print("your turn" + move + " where do you want to go? ")
 
