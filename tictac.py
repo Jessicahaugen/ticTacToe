cf5bba5b73cf4dbf12de40889e799815d0d70162
Jessica Haugen <j.rose.haugen@gmail.com>
2021-08-17 11:35:33 -0500

    tic tac toe dictionary model

---
+++ tictac.py
@@ -0,0 +1,15 @@
+board = {
+    '7': ' ' , '8': ' ' , '9': ' ' ,
+    '4': ' ' , '5': ' ' , '6': ' ' ,
+    '1': ' ' , '2': ' ' , '3': ' ' 
+}
+
+def displayBoard(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+displayBoard(board)
+
